abb8302d751685ee7b14e6a9c0784953ef24d683
Senal-Punsara <84228704+Senal-Punsara@users.noreply.github.com>
2021-05-16 21:51:06 +0530

    Update book.scala

--- Assignment1/Q3/book.scala
+++ Assignment1/Q3/book.scala
@@ -1,7 +1,13 @@
 object book extends App{
+def fun0(x:Int)=x;
+def fun1(x:Int):Double=(24.95*60.0/100.0)+3.0*x;
 
-def fun(x:Int):Double=(24.95*60.0/100.0)+3.0*x+(x-50.0)*0.75;
+def fun2(x:Int):Double=(24.95*60.0/100.0)+3.0*x+(x-50.0)*0.75;
 
-println(fun(60));
+  if (fun0(60)<=50){
+    println(fun1(60));
+  }else{
+    println(fun2(60));
+  }
 
 }
