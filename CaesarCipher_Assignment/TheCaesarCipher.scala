e2716fd2d089429e7e83b7dca60f7bdf45cabdb3
Senal Punsara <84228704+Senal-Punsara@users.noreply.github.com>
2021-08-09 18:07:57 +0530

    Add files via upload

---
+++ CaesarCipher_Assignment/TheCaesarCipher.scala
@@ -0,0 +1,23 @@
+//19001241
+object TheCaesarCipher extends App {
+//print("Enter a text: ")
+//val text:String=scala.io.StdIn.readLine();
+val text = "SenalPunsara";
+
+val alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+val Encryption=(c:Char,key:Int,alph:String)=>alph((alph.indexOf(c)+key)%alph.size)
+val Decryption=(c:Char,key:Int,alph:String)=>alph((alph.indexOf(c)-key)%alph.size);
+
+
+
+
+val cipher=(algo:(Char,Int,String)=>Char,text:String,key:Int,alph:String)=>text.map(algo(_,key,alph))
+val ct=cipher(Encryption,text,5,alphabet);
+val pt=cipher(Decryption,ct,5,alphabet);
+
+     println("your Text is    : "+text);
+     println("After Encryption: "+ct);
+     println("After Decryption: "+pt);
+
+}
+
